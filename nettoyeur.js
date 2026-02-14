7a4b61f095c01b4c971c49304da0a1ce6e2fe39c
KodK-fr <KodK_fr+code@proton.me>
2026-02-14 16:07:41 +0100

    Refactor tick function for cleaner pixel deletion

--- nettoyeur.js
+++ nettoyeur.js
@@ -9,17 +9,26 @@ elements.net = {
         var r = 1;
         for (var j = -r; j <= r; j++) {
             for (var i = -r; i <= r; i++) {
-                var coord = [pixel.x+i,pixel.y+j];
-                if (!isEmpty(coord[0],coord[1],true)) {
-                    var newPixel = pixelMap[coord[0]][coord[1]];
-                    if (newPixel.element!=="net" && 
-                        newPixel.element!=="insulator" && 
-                        newPixel.element!=="glass") {
-                        deletePixel(coord[0],coord[1]);
+                var x = pixel.x + i;
+                var y = pixel.y + j;
+
+                if (!isEmpty(x, y, true)) {
+                    var newPixel = pixelMap[x][y];
+
+                    if (
+                        newPixel &&
+                        newPixel.element !== "net" &&
+                        newPixel.element !== "insulator" &&
+                        newPixel.element !== "glass"
+                    ) {
+                        deletePixel(x, y);
                     }
                 }
             }
         }
-        if (Math.random()<0.02) deletePixel(pixel.x,pixel.y);
+
+        if (Math.random() < 0.02) {
+            deletePixel(pixel.x, pixel.y);
+        }
     }
 };
