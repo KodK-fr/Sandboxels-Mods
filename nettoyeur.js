d2679ce218ddf67780f8d47932bb169c92907b5b
KodK-fr <KodK_fr+code@proton.me>
2026-02-16 15:00:56 +0100

    Replace net element with Disolveur_universel

--- nettoyeur.js
+++ nettoyeur.js
@@ -1,34 +1,6 @@
-elements.net = {
-    color: "#aa00ff",
+elements.Disolveur_universel = {
+    color: "#ffff00",
     behavior: behaviors.LIQUID,
-    category: "special",
-    state: "liquid",
-    density: 1000,
-    desc: "Supprime tout sauf insulator et glass",
-    tick: function(pixel) {
-        var r = 1;
-        for (var j = -r; j <= r; j++) {
-            for (var i = -r; i <= r; i++) {
-                var x = pixel.x + i;
-                var y = pixel.y + j;
-
-                if (!isEmpty(x, y, true)) {
-                    var newPixel = pixelMap[x][y];
-
-                    if (
-                        newPixel &&
-                        newPixel.element !== "net" &&
-                        newPixel.element !== "insulator" &&
-                        newPixel.element !== "glass"
-                    ) {
-                        deletePixel(x, y);
-                    }
-                }
-            }
-        }
-
-        if (Math.random() < 0.02) {
-            deletePixel(pixel.x, pixel.y);
-        }
-    }
-};
+    category: "liquids",
+    state: "liquid"
+}
